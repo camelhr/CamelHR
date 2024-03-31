b7e31c420ffc3f5dc7b35905196581db0d81988c
vivekweb2013 <vivekweb2013@gmail.com>
2024-03-31 10:04:31 +0530

    Use arrow function for app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { FC } from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App: FC = () => {
   const [count, setCount] = useState(0)
 
   return (
