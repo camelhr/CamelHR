353b93ccd1972228bf2c904a0eadbbab3f859116
vivekweb2013 <vivekweb2013@gmail.com>
2024-06-18 20:27:27 +0530

    Setup proxy for local development

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,32 @@
 /// <reference types="vite/client" />
 
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 4242,
-    strictPort: true,
-  },
-  test: {
-    coverage: {
-      provider: 'istanbul',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 4242,
+      strictPort: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_PROXY_TARGET || 'http://localhost:8080',
+          changeOrigin: true,
+        },
+      },
+      cors: true,
     },
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/setupTest.ts'],
-  },
+    test: {
+      coverage: {
+        provider: 'istanbul',
+      },
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/setupTest.ts'],
+    },
+  }
 })
